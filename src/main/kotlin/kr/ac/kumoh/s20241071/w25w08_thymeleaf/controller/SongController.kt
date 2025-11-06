0a53d9195bbd415b825af8ccfa5602d79b3c2d91
tjsdn0520 <tjsdn0530@gmail.com>
2025-11-06 17:24:50 +0900

    🏗️Controller에서 Service 사용

--- src/main/kotlin/kr/ac/kumoh/s20241071/w25w08_thymeleaf/controller/SongController.kt
+++ src/main/kotlin/kr/ac/kumoh/s20241071/w25w08_thymeleaf/controller/SongController.kt
@@ -1,14 +1,17 @@
 package kr.ac.kumoh.s20241071.w25w08_thymeleaf.controller
 
+import kr.ac.kumoh.s20241071.w25w08_thymeleaf.service.SongService
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 
 @Controller
-class SongController {
+class SongController(
+    private val service: SongService
+) {
     @GetMapping("/songs/random")
     fun getRandomSong(model: Model): String {
-        model.addAttribute("songTitle","Soda Pop")
+        model.addAttribute("songTitle",service.getRandomSong()?.title?: "노래 없음")
         return "random"
     }
 }
